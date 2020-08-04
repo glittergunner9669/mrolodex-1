8005d19346ba40a2804796a47325d4ab1d7222db
Mabunay <mabunaygo@MNLPREPDSN2WSA.local>
2020-08-04 22:17:16 +0800

    Updates

---
+++ precache-manifest.b2ee59b77ab38f3312db9affb1b2a30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e667b3aa5740b08ae8472a007e5c4329",
+    "url": "/mrolodex-1/index.html"
+  },
+  {
+    "revision": "6ebc538dc844c4dfe254",
+    "url": "/mrolodex-1/static/css/main.6e03cdcf.chunk.css"
+  },
+  {
+    "revision": "fba924f57a2573baf74e",
+    "url": "/mrolodex-1/static/js/2.d494b07b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrolodex-1/static/js/2.d494b07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ebc538dc844c4dfe254",
+    "url": "/mrolodex-1/static/js/main.0d03b359.chunk.js"
+  },
+  {
+    "revision": "7a7321e3fe9ac5213643",
+    "url": "/mrolodex-1/static/js/runtime-main.7c39e2ff.js"
+  }
+]);
